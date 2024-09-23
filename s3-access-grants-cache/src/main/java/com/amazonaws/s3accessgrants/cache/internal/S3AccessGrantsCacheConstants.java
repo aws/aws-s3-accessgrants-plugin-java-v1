7045e02be68638f017ef9503c0d2407d3be309ab
Nikita Manish Sawant <nmsawant@amazon.com>
2024-09-23 09:21:06 -0400

    added user agent to internal clients and updated cache ttl.

--- s3-access-grants-cache/src/main/java/com/amazonaws/s3accessgrants/cache/internal/S3AccessGrantsCacheConstants.java
+++ s3-access-grants-cache/src/main/java/com/amazonaws/s3accessgrants/cache/internal/S3AccessGrantsCacheConstants.java
@@ -18,7 +18,7 @@ package com.amazonaws.s3accessgrants.cache.internal;
 public class S3AccessGrantsCacheConstants {
     public static final int DEFAULT_ACCOUNT_ID_MAX_CACHE_SIZE = 1_000;
     public static final int MAX_LIMIT_ACCOUNT_ID_MAX_CACHE_SIZE = 1_000_000;
-    public static final int DEFAULT_ACCOUNT_ID_EXPIRE_CACHE_AFTER_WRITE_SECONDS = 6_00; // 10 mins
+    public static final int DEFAULT_ACCOUNT_ID_EXPIRE_CACHE_AFTER_WRITE_SECONDS = 3_600; // 1 hour
     public static final int MAX_LIMIT_ACCOUNT_ID_EXPIRE_CACHE_AFTER_WRITE_SECONDS = 2_592_000; // 30 days
 
     public static final int DEFAULT_ACCESS_GRANTS_MAX_CACHE_SIZE = 30_000;
@@ -30,6 +30,6 @@ public class S3AccessGrantsCacheConstants {
 
     public static final int BUCKET_REGION_CACHE_SIZE = 1_000;
     public static final int MAX_BUCKET_REGION_CACHE_SIZE = 1_000_000;
-    public static final int MAX_BUCKET_REGION_EXPIRE_CACHE_AFTER_WRITE_SECONDS = 6_00; // 10 mins
-    public static final int BUCKET_REGION_EXPIRE_CACHE_AFTER_WRITE_SECONDS= 3_00; // 5 mins
+    public static final int BUCKET_REGION_EXPIRE_CACHE_AFTER_WRITE_SECONDS = 3_600; // 1 hour
+    public static final int MAX_BUCKET_REGION_EXPIRE_CACHE_AFTER_WRITE_SECONDS= 86_400; // 1 day
 }
